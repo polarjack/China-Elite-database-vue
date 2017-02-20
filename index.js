820ec832ac72d25e633bdce4aa7bbb0b0bad7e99
POLARGG <luckyjack85@gmail.com>
2017-02-21 04:41:24 +0800

    one card version

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+Vue.component('todo-item', {
+  template: '\
+  <div class="collection-item">\
+  {{title}}\
+  <i class="material-icons right" v-on:click="$emit(\'remove\')" >delete</i>\
+  </div>\
+  ',
+  props: ['title']
+})
 
+new Vue({
+  el: '#todo-list-example',
+  data: {
+    newTodoText: '',
+    todos: [
+      'first',
+      'second',
+      'third'
+    ]
+  },
+  methods: {
+    addNewTodo: function() {
+      this.todos.push(this.newTodoText)
+      this.newTodoText = ''
+    }
+  }
+})
